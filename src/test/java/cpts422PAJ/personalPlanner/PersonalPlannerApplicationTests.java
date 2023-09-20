ae402054dbf3c741d86aaa18653455f6d190ae06
peter-t-wang315 <peter.wang2@wsu.edu>
2023-09-19 23:03:27 -0700

    Added spring initializing code to project

---
+++ src/test/java/cpts422PAJ/personalPlanner/PersonalPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package cpts422PAJ.personalPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
