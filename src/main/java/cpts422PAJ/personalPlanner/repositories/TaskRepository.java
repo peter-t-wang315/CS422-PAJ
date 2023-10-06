1f02a18ab7e75c4d985c4fa71f8affb11c3cfa6a
peter-t-wang315 <wang.t.peter@gmail.com>
2023-10-05 23:22:58 -0700

    Fixed a bug

--- src/main/java/cpts422PAJ/personalPlanner/repositories/TaskRepository.java
+++ src/main/java/cpts422PAJ/personalPlanner/repositories/TaskRepository.java
@@ -3,11 +3,12 @@ package cpts422PAJ.personalPlanner.repositories;
 import cpts422PAJ.personalPlanner.entities.Task;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface TaskRepository extends CrudRepository<Task,Long> {
     @Query("SELECT t FROM Task t WHERE t.u.id = :userId")
-    List<Task> findTasksByUserId(Long userId);
+    List<Task> findTasksByUserId(@Param("userId")Long userId);
 }
